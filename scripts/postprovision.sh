90c035d02802dd68acf41d0a6c69d7daa5f0c1c5
Dominique Broeglin <dominique.broeglin@microsoft.com>
2024-12-15 15:56:38 +0100

    Do not try to add Container App URL to identifier-uris (does not work when domain is not verified in the tenant)

--- scripts/postprovision.sh
+++ scripts/postprovision.sh
@@ -9,5 +9,4 @@ echo "Adding app registration redirect URI $REDIRECT_URI..."
 az ad app update \
     --id "$AZURE_CLIENT_APP_ID" \
     --web-redirect-uris "http://localhost:5801/.auth/login/aad/callback" "$REDIRECT_URI" \
-    --identifier-uris "api://$AZURE_CLIENT_APP_ID" "$SERVICE_BACKEND_URL" \
     --output table
